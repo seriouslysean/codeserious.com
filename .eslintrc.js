c7568bab8047608c520b7f4aa43b413789059745
Sean Kennedy <skennedy@urbn.com>
2021-01-23 01:01:25 -0500

    Switch from Nuxt to 11ty

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,6 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-    },
-    parserOptions: {
-      parser: 'babel-eslint',
-    },
-    extends: [
-      '@nuxtjs',
-      'prettier',
-      'prettier/vue',
-      'plugin:prettier/recommended',
-      'plugin:nuxt/recommended',
-    ],
-    plugins: [
-      'prettier',
-    ],
+    extends: 'airbnb-base',
     rules: {
-      'nuxt/no-cjs-in-config': 'off',
-      'prettier/prettier': 'error',
-      'semi': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
+        indent: ['error', 4],
     },
-  }
+};
